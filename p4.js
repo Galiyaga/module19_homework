68402e4e5a5724b6371481e1192db67b9c23e76f
Galiyga <galiyaga@yandex.ru>
2023-09-27 16:19:23 +0300

    использование принципа SOLID на файлу JS

--- p4.js
+++ p4.js
@@ -1,58 +1,70 @@
-function Device(name, power) {
-    this.name = name;
-    this.power = power;
-    this.isOn = false;
-}
-
-Device.prototype.turnOn = function() {
-    this.isOn = true;
-    console.log(this.name + ' is turned on.');
-}
-
-Device.prototype.turnOff = function() {
-    this.isOn = false;
-    console.log(this.name + ' is turned off.');
-}
-
-function Lamp(brightness, name, power) {
-    this.brightness = brightness;
-    this.name = name;
-    this.power = power;
-}
-
-Lamp.prototype = new Device();
-
-Lamp.prototype.makeDimmer = function() {
-    if (this.brightness > 0) {
-        this.brightness -= 10;
-        return "Brightness: " + this.brightness
+class Device {
+    constructor(name, power) {
+        this.name = name;
+        this.power = power;
+        this.turnOnOff = new TurnOnOff();
     }
 }
 
-Lamp.prototype.makeBrighter = function() {
-    if (this.brightness < 100) {
-        this.brightness += 10;
-        return "Brightness: " + this.brightness
+class TurnOnOff {
+    constructor() {
+        this.isOn = false;
+    }
+    turnOn() {
+        this.isOn = true;
+        console.log('Device is turned on.');
+    }
+    turnOff() {
+        this.isOn = false;
+        console.log('Device is turned off.');
     }
 }
 
-function Computer(memory, name, power) {
-    this.memory = memory;
-    this.name = name;
-    this.power = power;
-    this.CDromOpened = false;
+class Lamp extends Device {
+    constructor(brightness, name, power) {
+        super(name, power);
+        this.brightnessControl = new BrightnessControl(brightness);
+    }
 }
 
-Computer.prototype = new Device();
-
-Computer.prototype.openCDrom = function() {
-    this.CDromOpened = true;
-    return "CDrom is opened"
+class BrightnessControl {
+    constructor(brightness) {
+        this.brightness = brightness;
+    }
+    makeDimmer() {
+        if (this.brightness > 0) {
+            this.brightness -= 10;
+            return "Brightness: " + this.brightness
+        }
+    }
+    makeBrighter() {
+        if (this.brightness < 100) {
+            this.brightness += 10;
+            return "Brightness: " + this.brightness
+        }
+    }
 }
 
-Computer.prototype.closeCDrom = function() {
-    this.CDromOpened = false;
-    return "CDrom is closed"
+class Computer extends Device {
+    constructor(memory, name, power) {
+        super(name, power);
+        this.memory = memory;
+        this.cdRomControl = new CdRomControl();
+    }
+}
+
+class CdRomControl {
+    constructor() {
+        this.CDromOpened = false;
+    }
+    openCDrom() {
+        this.CDromOpened = true;
+        return "CDrom is opened"
+    }
+    closeCDrom() {
+        this.CDromOpened = false;
+        return "CDrom is closed"
+    }
 }
 
 let lamp = new Lamp (50, "My lamp", 20)
@@ -60,5 +72,3 @@ let computer = new Computer(16, "My computer", 2000)
 
 console.log("lamp: ", lamp, lamp.makeBrighter()) 
 console.log("computer: ", computer, computer.openCDrom()) 
-
-
