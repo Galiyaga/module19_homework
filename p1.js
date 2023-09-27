d42b49e247e0222fd3054fa2fdb2f198f0aab443
Galiyga <galiyaga@yandex.ru>
2023-09-27 16:32:56 +0300

    использование принципа DRY на файле JS

--- p1.js
+++ p1.js
@@ -1,16 +1,15 @@
 function ownProperties(obj) {
-    const propertyNames = Object.getOwnPropertyNames(obj);
-    for (const propName of propertyNames) {
-      if (obj.hasOwnProperty(propName)) {
-        console.log(`Key: ${propName}, Value: ${obj[propName]}`);
-      }
-    }
+  const propertyNames = Object.getOwnPropertyNames(obj);
+  for (const propName of propertyNames) {
+    console.log(`Key: ${propName}, Value: ${obj[propName]}`);
   }
+}
 
 const person = {
-    name: 'Ivan',
-    age: 30,
-    city: 'Moscow',
-  };
-  
+  name: 'Ivan',
+  age: 30,
+  city: 'Moscow',
+};
+
 ownProperties(person);
+2
